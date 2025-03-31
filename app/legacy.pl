15d92f78103a4c6b63d251f266812ce1a1ec03b3
Sage <slothlysage@gmail.com>
2025-03-30 17:30:09 -0700

    perl file, docker container, deployment, and service

---
+++ app/legacy.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+
+print "Content-type: text/html\n\n";
+print "<html><head><title>Legacy Perl DB System</title></head>";
+print "<body><h1>Welcome to the Legacy Perl DB System</h1>";
+print "<p>This simulates a legacy Perl-based database interface.</p>";
+print "</body></html>";
